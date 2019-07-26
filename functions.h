4ec8395fe8fd4e55946ca6874c6efdb81322c920
Matt Peachey <matthew_peachey@hotmail.com>
2019-07-26 06:57:49 -0300

    Added functions.h and moved function prototypes to that file.  Removed unnecessary code from NeoPixel2048.ino

---
+++ functions.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTIONS
+  #define FUNCTIONS
+
+  //Function prototypes
+
+  void up();
+  void down();
+  void left();
+  void right();
+
+  // HELPER FUNCTION TO RETURN RANDOM RGB VALUE
+  void randomColor(){
+    int r = random(151);
+    int g = random(151);
+    int b = random(151);
+    //TODO: Return the associated HEX value.
+  }
+  
+#endif
